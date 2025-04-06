24bfe1da3b417db380f709ea6c5024bf531a45bb
NullByte3 <122549857+NullByte3@users.noreply.github.com>
2025-04-06 16:53:59 +0000

    Support for public shit

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import api from './api/index.js';
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/v1', api);
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 export default app;
