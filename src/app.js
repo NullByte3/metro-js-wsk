81fc7d41b098a84f46116e2f7f3f0a0b3b297c9a
NullByte3 <122549857+NullByte3@users.noreply.github.com>
2025-04-06 17:28:06 +0000

    pubg sucks

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import api from './api/index.js';
 import { notFoundHandler, errorHandler } from './middlewares.js';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
@@ -13,5 +16,7 @@ app.use('/uploads', express.static('uploads'));
 app.use('/api/v1', api);
 app.use(notFoundHandler);
 app.use(errorHandler);
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 export default app;
